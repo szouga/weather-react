9bf36f110cfd648be4537417c085c833087d32f5
szouga <szouga9@gmail.com>
2026-09-13 20:26:39 -0500

    update search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-// import WeatherInfo from "./WeatherInfo";
+import WeatherInfo from "./WeatherInfo";
 // import WeatherForecast from "./WeatherForecast";
 // import "./Weather.css";
 // import sheCodesLogo from "./shecodes_logo.png";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 export default function Weather(props) {
   // const [ready, setReady] = useState(false);
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   // const [temperature, setTemperature] = useState(null);
@@ -25,11 +25,24 @@ export default function Weather(props) {
       city: response.data.city,
     });
   }
-
+  function search() {
+    const apiKey = "7e1fbo71a48539tbb0610fa3a35820ef";
+    // let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    //search for a city
+    search();
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -37,6 +50,7 @@ export default function Weather(props) {
                 placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -48,44 +62,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "7e1fbo71a48539tbb0610fa3a35820ef";
-    // let city = "New York";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,40 +1,73 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
+// import WeatherIcon from "./WeatherIcon";
 import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
       <div className="row mt-3">
-        {/* Left Column: City and Meta Details */}
         <div className="col-6">
-          <h1>{props.data.city}</h1>
-          <ul className="ps-0 list-unstyled">
-            <li>
-              <FormattedDate date={props.data.date} />, {props.data.description}
-            </li>
-            <li>
-              Humidity:{" "}
-              <span className="text-danger">{props.data.humidity}%</span>, Wind:{" "}
-              <span className="text-danger">{props.data.wind}km/h</span>
-            </li>
-          </ul>
-        </div>
-
-        {/* Right Column: Icon and Temperature matched exactly to col-6 */}
-        <div className="col-6 d-flex justify-content-end align-items-center">
-          <div className="temperature-container d-flex align-items-center">
-            <WeatherIcon code={props.data.icon} size={52} />
-            <div className="d-flex align-items-start">
-              <span className="temperature fw-bold lh-1">
+          <div className="clearfix">
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">
                 {Math.round(props.data.temperature)}
               </span>
-              <span className="unit fs-4">°C</span>
+              <span className="unit">°C</span>
             </div>
           </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
     </div>
   );
 }
+//   <div className="WeatherInfo">
+//     <div className="row mt-3">
+//       {/* Left Column: City and Meta Details */}
+//       <div className="col-6">
+//         <h1>{props.data.city}</h1>
+//         <ul className="ps-0 list-unstyled">
+//           <li>
+//             <FormattedDate date={props.data.date} />, {props.data.description}
+//           </li>
+//           <li>
+//             Humidity:{" "}
+//             <span className="text-danger">{props.data.humidity}%</span>, Wind:{" "}
+//             <span className="text-danger">{props.data.wind}km/h</span>
+//           </li>
+//         </ul>
+//       </div>
+
+//       {/* Right Column: Icon and Temperature matched exactly to col-6 */}
+//       <div className="col-6 d-flex justify-content-end align-items-center">
+//         <div className="temperature-container d-flex align-items-center">
+//           <WeatherIcon code={props.data.icon} size={52} />
+//           <div className="d-flex align-items-start">
+//             <span className="temperature fw-bold lh-1">
+//               {Math.round(props.data.temperature)}
+//             </span>
+//             <span className="unit fs-4">°C</span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
